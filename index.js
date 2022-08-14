13898f1ecde1a7d62c111bdb2572e2cc11b955ae
shweta0318 <64451811+shweta0318@users.noreply.github.com>
2022-08-14 21:00:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node22,deployed by shweta!\n'
+  const msg = 'Hello Node22,deployed by mentorkart trainer!\n'
   res.end(msg);
 });
 
