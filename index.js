7a3b37b79efa8d69df8f04823fc7911a84fba7d8
kumarsgithub07 <109962975+kumarsgithub07@users.noreply.github.com>
2022-09-02 12:48:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello World Node22,deployed by Subhasisha-DevOps-Engineer!\n'
+  const msg = 'Hello World Node22,deployed by Subhasisha-Certified-DevOps-Engineer!\n'
   res.end(msg);
 });
 
